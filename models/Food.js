1bc809d75c7e051a4adbe20ae03655a886055480
afrique18 <vincentv1225@gmail.com>
2023-06-26 22:58:56 -0400

    Update Food.js file

---
+++ models/Food.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const macros = [
+    { name: 'Carbohydrates', description: 'A major source of energy for the body.' },
+    { name: 'Protains', description: 'Essential for building and reparing tissues and supporting various bodily functions.' },
+    { name: 'Fats', description: 'Provide energy, aid in nutrient absorption, and support cell growth'}
+];
+
+app.get('/macros', (req, res) => {
+    res.json(macros);
+});
+
+const mealPlan = [
+    { name: 'Breakfast', foods: ['Oatmeal', 'Eggs', 'Avocado'] },
+    { name: 'Lunch', foods: ['Chicken Breast', 'Brown Rice', 'Brocolli'] },
+    { name: 'Dinner', foods: ['Salmon', 'Quinoa', 'Asparagus'] }
+];
+
+app.get('/meal-plan', (req, res) => {
+    res.json(mealPlan);
+});
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+})
