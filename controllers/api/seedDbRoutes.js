8edf35831901214cdf1d36a3e3f7aa65ac79820d
Lalu423 <>
2023-07-02 19:17:58 -0400

    login/logout js

---
+++ controllers/api/seedDbRoutes.js
@@ -0,0 +1,23 @@
+const sequealize = require('../config/connection');
+const { User, Workout, Food } = require('../models');
+
+const userData = require('./userData.json');
+const wokroutData = require('./workoutData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const project of projectData) {
+    await Project.create({
+      ...project,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+};
+
+module.exports = seedDatabase;
