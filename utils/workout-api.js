94e3120a6af9e6810edd64e511c40b109ff73067
Lalu423 <>
2023-07-03 20:37:25 -0400

    fixed workout route and api

--- utils/workout-api.js
+++ utils/workout-api.js
@@ -1,23 +1,44 @@
-const workout = async function populateDropdown() { 
+const workout = async function workoutData() {
+  try {
+    const response = await fetch("https://wger.de/api/v2/exercise", {
+      method: "GET",
+      headers: { Authorization: "Token 56c76d8178d0eee843d444734daf71278e39d1ab" }
+    });
 
-  const response = await fetch("https://wger.de/api/v2/exercise", {
-    method: "GET",
-    headers: {Authorization: "Token 56c76d8178d0eee843d444734daf71278e39d1ab"}
-});
-    
-  const jsonData = await response.json();
-  //console.log(jsonData);
-  const dropDown = document.getElementById("exerciseDropdown");
+    if (!response.ok) {
+      throw new Error("Request failed with status " + response.status);
+    }
 
-  jsonData.results.forEach((exercise) => {
-    const option = document.createElement("option");
-    option.text = exercise.name;
-    option.value = exercise.id;
-    dropDown.appendChild(option);
-  });
-
-  return jsonData;
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error; {
+      throw new Error('Failed to fetch workout data' + error.message);
+    }
+  }
 };
+
+workout();
+// const workout = async function workoutData() { 
+
+//   const response = await fetch("https://wger.de/api/v2/exercise", {
+//     method: "GET",
+//     headers: {Authorization: "Token 56c76d8178d0eee843d444734daf71278e39d1ab"}
+// });
+    
+//   const jsonData = await response.json();
+//   //console.log(jsonData);
+//   const dropDown = document.getElementById("exerciseDropdown");
+
+//   jsonData.results.forEach((exercise) => {
+//     const option = document.createElement("option");
+//     option.text = exercise.name;
+//     option.value = exercise.id;
+//     dropDown.appendChild(option);
+//   });
+
+//   return jsonData;
+// };
 
 
 
