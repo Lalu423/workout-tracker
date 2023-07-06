9161df4ae9b9e8932fe18a4c8f94b46c1668422a
Lalu423 <>
2023-07-05 20:37:59 -0400

    fixed issue

--- models/Food.js
+++ models/Food.js
@@ -1,30 +1,30 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection')
+// const { Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/connection')
 
-class Food extends Model {}
+// class Food extends Model {}
 
-const macros = [
-    { name: 'Carbohydrates', description: 'A major source of energy for the body.' },
-    { name: 'Protains', description: 'Essential for building and reparing tissues and supporting various bodily functions.' },
-    { name: 'Fats', description: 'Provide energy, aid in nutrient absorption, and support cell growth'}
-];
+// const macros = [
+//     { name: 'Carbohydrates', description: 'A major source of energy for the body.' },
+//     { name: 'Protains', description: 'Essential for building and reparing tissues and supporting various bodily functions.' },
+//     { name: 'Fats', description: 'Provide energy, aid in nutrient absorption, and support cell growth'}
+// ];
 
-app.get('/macros', (req, res) => {
-    res.json(macros);
-});
+// // app.get('/macros', (req, res) => {
+// //     res.json(macros);
+// // });
 
-const mealPlan = [
-    { name: 'Breakfast', foods: ['Oatmeal', 'Eggs', 'Avocado'] },
-    { name: 'Lunch', foods: ['Chicken Breast', 'Brown Rice', 'Brocolli'] },
-    { name: 'Dinner', foods: ['Salmon', 'Quinoa', 'Asparagus'] }
-];
+// const mealPlan = [
+//     { name: 'Breakfast', foods: ['Oatmeal', 'Eggs', 'Avocado'] },
+//     { name: 'Lunch', foods: ['Chicken Breast', 'Brown Rice', 'Brocolli'] },
+//     { name: 'Dinner', foods: ['Salmon', 'Quinoa', 'Asparagus'] }
+// ];
 
-app.get('/meal-plan', (req, res) => {
-    res.json(mealPlan);
-});
+// // app.get('/meal-plan', (req, res) => {
+// //     res.json(mealPlan);
+// // });
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
-})
+// // app.listen(3000, () => {
+// //     console.log('Server started on port 3000');
+// // })
 
-module.exports = Food;
+// module.exports = Food;
