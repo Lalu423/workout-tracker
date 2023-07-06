204c09570b9d67783eb0c38ae35e369f6b2f3951
Yandra Kamberaj <yandrakamb@gmail.com>
2023-07-06 12:51:56 -0400

    had to fix some j son

--- utils/workout-api.js
+++ utils/workout-api.js
@@ -1,17 +1,19 @@
 const workout = async () => {
+  alert("here");
   try {
     const response = await fetch("https://wger.de/api/v2/exercise", {
       method: "GET",
       headers: { Authorization: "Token 56c76d8178d0eee843d444734daf71278e39d1ab" }
     });
-
+    alert("here");
     if (!response.ok) {
       throw new Error("Request failed with status " + response.status);
     }
 
     const data = await response.json();
     console.log(data);
-    return response;
+    alert("here");
+    // return response;
   } catch (error) {
     console.error('Error:', error);
     throw new Error('Failed to fetch workout data: ' + error.message);
@@ -45,7 +47,7 @@ const workout = async () => {
 
 
 
-module.exports = { workout }
+// module.exports = { workout }
 
 
 // attempt number #3
